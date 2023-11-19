7a812f2ad1412b8f21db3b89a1b532dc1d6e2257
israel <Gerardo@skyline.com>
2023-11-19 00:10:39 -0800

    HaspMap Practica6

--- Practica6.java
+++ Practica6.java
@@ -10,15 +10,14 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.Map;
 
 public class Practica6 {
 
     public static void main(String[] args) {
         try {
-            HashMap<String, String> chuckNorrisJokeMap = obtenerChuckNorrisJoke();
-            String textoChiste = obtenerTextoDelChiste(chuckNorrisJokeMap.get("chiste"));
-            String urlChiste = chuckNorrisJokeMap.get("url");
+            HashMap<String, String> mapaChisteChuckNorris = obtenerChisteChuckNorris();
+            String textoChiste = obtenerTextoDelChiste(mapaChisteChuckNorris.get("chiste"));
+            String urlChiste = mapaChisteChuckNorris.get("url");
 
             System.out.println("Chiste de Chuck Norris: " + textoChiste);
             System.out.println("URL del chiste: " + urlChiste);
@@ -27,47 +26,47 @@ public class Practica6 {
         }
     }
 
-    private static HashMap<String, String> obtenerChuckNorrisJoke() throws IOException {
+    private static HashMap<String, String> obtenerChisteChuckNorris() throws IOException {
         String apiUrl = "https://api.chucknorris.io/jokes/random";
         URL url = new URL(apiUrl);
 
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
 
-        int responseCode = connection.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            StringBuilder response = new StringBuilder();
-            String line;
+        int codigoRespuesta = connection.getResponseCode();
+        if (codigoRespuesta == HttpURLConnection.HTTP_OK) {
+            BufferedReader lector = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder respuesta = new StringBuilder();
+            String linea;
 
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
+            while ((linea = lector.readLine()) != null) {
+                respuesta.append(linea);
             }
 
-            reader.close();
+            lector.close();
 
             // Parsear la respuesta JSON y almacenarla en un HashMap
-            HashMap<String, String> chuckNorrisJokeMap = new HashMap<>();
-            chuckNorrisJokeMap.put("chiste", response.toString());
-            chuckNorrisJokeMap.put("url", obtenerUrlDelChiste(response.toString()));
+            HashMap<String, String> mapaChisteChuckNorris = new HashMap<>();
+            mapaChisteChuckNorris.put("chiste", respuesta.toString());
+            mapaChisteChuckNorris.put("url", obtenerUrlDelChiste(respuesta.toString()));
 
-            return chuckNorrisJokeMap;
+            return mapaChisteChuckNorris;
         } else {
-            throw new IOException("Error al obtener el chiste de Chuck Norris. Código de respuesta: " + responseCode);
+            throw new IOException("Error al obtener el chiste de Chuck Norris. Código de respuesta: " + codigoRespuesta);
         }
     }
 
     private static String obtenerTextoDelChiste(String respuestaJson) {
         // Parsear la respuesta JSON y obtener el valor del campo "value"
-        int startIndex = respuestaJson.indexOf("\"value\":\"") + 9;
-        int endIndex = respuestaJson.indexOf("\"}", startIndex);
-        return respuestaJson.substring(startIndex, endIndex);
+        int inicio = respuestaJson.indexOf("\"value\":\"") + 9;
+        int fin = respuestaJson.indexOf("\"}", inicio);
+        return respuestaJson.substring(inicio, fin);
     }
 
     private static String obtenerUrlDelChiste(String respuestaJson) {
         // Parsear la respuesta JSON y obtener el valor del campo "url"
-        int startIndex = respuestaJson.indexOf("\"url\":\"") + 7;
-        int endIndex = respuestaJson.indexOf("\",\"value\"", startIndex);
-        return respuestaJson.substring(startIndex, endIndex);
+        int inicio = respuestaJson.indexOf("\"url\":\"") + 7;
+        int fin = respuestaJson.indexOf("\",\"value\"", inicio);
+        return respuestaJson.substring(inicio, fin);
     }
 }
